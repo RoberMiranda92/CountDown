1bff8257ee15a62607afd7dad827b8756e718e43
Roberto Miranda <rmp0046@gmail.com>
2020-04-03 17:51:59 -0300

    Feature: Create Event screen (#1)  (#8)
    * Init of create event fragment
    
    * Configure view model and view model factory
    
    * Display Date Selector and Time Selector
    
    * Fix clock theme

---
+++ app/src/main/java/com/robertomiranda/countdown/createevent/CreateEventFragment.kt
@@ -0,0 +1,46 @@
+package com.robertomiranda.countdown.createevent
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+import com.robertomiranda.countdown.databinding.FragmentCreateEventBinding
+
+class CreateEventFragment : Fragment() {
+
+
+    private lateinit var binding: FragmentCreateEventBinding
+    private val viewModel: CreateEventViewModel by viewModels { ViewModelFactory.getInstance() }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        binding = FragmentCreateEventBinding.inflate(inflater)
+
+        return with(binding) {
+            vm = viewModel
+            setUpListeners()
+            root
+        }
+    }
+
+    private fun setUpListeners() {
+        binding.datePicker.setOnClickListener {
+            showDatePicker(parentFragmentManager) { date ->
+                (it as TextInputEditText).setText(date)
+            }
+        }
+
+        binding.timePicker.setOnClickListener {
+            showTimePicker(it.context) { time ->
+                (it as TextInputEditText).setText(time)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/robertomiranda/countdown/createevent/CreateEventViewModel.kt
@@ -0,0 +1,7 @@
+package com.robertomiranda.countdown.createevent
+
+import androidx.lifecycle.ViewModel
+
+class CreateEventViewModel : ViewModel() {
+
+}
