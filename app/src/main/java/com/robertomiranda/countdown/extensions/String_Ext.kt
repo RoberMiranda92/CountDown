fd64e904e519e93f33a93388c1675cf2981844cf
Roberto <rmp0046@gmail.com>
2020-04-05 18:48:54 -0400

    Check if date is valid

--- app/src/main/java/com/robertomiranda/countdown/extensions/String_Ext.kt
+++ app/src/main/java/com/robertomiranda/countdown/extensions/String_Ext.kt
@@ -1,4 +1,14 @@
 package com.robertomiranda.countdown.extensions
 
+import java.text.SimpleDateFormat
+import java.util.*
+
 inline fun String?.ifEmptyOrNull(defaultValue: () -> String): String =
     if (this.isNullOrEmpty()) defaultValue() else this
+
+fun String.toDate(format: String, locale: Locale = Locale.getDefault()): Date? = SimpleDateFormat(format, locale).parse(this)
+
+fun Date.toString(format: String, locale: Locale = Locale.getDefault()): String {
+    val formatter = SimpleDateFormat(format, locale)
+    return formatter.format(this)
+}
